8783958e5941275c7b0f117139781f7c3e2cc3b7
Siju Samuel <sijusamuel@gmail.com>
2019-08-09 05:05:24 +0000

    Synced for local update from master/origin

--- python/tvm/relay/qnn/op/__init__.py
+++ python/tvm/relay/qnn/op/__init__.py
@@ -1,4 +1,3 @@
-#!/bin/bash
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -15,17 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
-set -e
-set -u
-set -o pipefail
-
-# The https:// source added below required an apt https transport
-# support.
-apt-get install apt-transport-https
-
-# Install the necessary dependencies for Chisel
-echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-apt-get update
-apt-get install -y verilator sbt
+# pylint: disable=wildcard-import
+"""Neural network related operators."""
+from __future__ import absolute_import as _abs
+from .qnn import *
